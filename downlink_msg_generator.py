c916cb766135608e84916fb1c59efb05d99e9c4a
danparseg <daniel.parra.segovia@gmail.com>
2022-04-22 13:58:21 +0200

    aaaaaa

---
+++ downlink_msg_generator.py
@@ -0,0 +1,7 @@
+"""
+downlink msg:
+barometro(altura, presion), aceleracion, giroscopio, magnetometro, gps, 3 inas, 3 sensores distancia, separado?, transmitiendo?, grabando?
+"""
+
+def downlink_msg_generator():
+    
