2242eb9a28cb634a25385a4ae81413f8973532ed
AminYassin <aminyassin19@gmail.com>
2022-06-09 17:16:30 +0200

    creado

---
+++ laser_sensor.py
@@ -0,0 +1,27 @@
+"""
+int sensor1 int sensor2 int sensor3
+returns true/false if separated or not
+"""
+def is_separated(s1,s2,s3):
+	#make sure the inputs aren't weird
+	#are they ints? are they positive?
+	#if they are weird, log an error msg
+	
+	#decide which is the most reliable sensor
+	distance=most_reliable_sensor
+	
+	#is it separated?
+	if(distance<threshold):
+		return False
+    return True
+    
+    
+"""
+reads from distance sensors
+returns true/false if separated or not
+"""
+def read_is_separated():
+	#read s1
+	#read s2
+	#read s3
+	return is_separated(s1,s2,s3)
