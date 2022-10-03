0361e711c47d4aad1922c56f7d515ca87ec09960
cristiansgc <80602985+cristiansgc@users.noreply.github.com>
2022-10-03 11:05:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,22 +7,15 @@ from GPS import log_GPS
 from camera import take_caption, take_video
 from solenoid import deploy
 
-
 ## get initial time
 t0 = time()
 
 while True:
 
     log_ina(t0)
-
     log_laser_sensor(t0)
-
     log_barometer(t0)
-
     log_GPS(t0)
-
     take_caption(t0)
-
     take_video(t0)
-
     deploy(t0)
